186261cde24e8cef64b2391bfd061877c738cf2f
Rakesh Pradhan <rp5865442@gmail.com>
2025-10-27 10:44:25 +0530

    add new exprience

--- src/components/main/Experience.jsx
+++ src/components/main/Experience.jsx
@@ -42,6 +42,45 @@ export function Experience() {
         </div>
       ),
     },
+    {
+      title: "early 2025",
+      content: (
+        <div>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            <span className="font-bold text-lg">Backend Developer(SDE-I) </span> <br />
+            <span className="font-generalsans">PeerHub</span><br />
+            As part of a collaborative team, contributed to the development of multiple projects like social media application and content management system's.
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <img
+              src="/peerHub1.png"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <img
+              src="/peerHub2.png"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <img
+              src="/peerHub3.png"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <img
+              src="/peerHub4.png"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+          </div>
+        </div>
+      ),
+    },
   ]
   return (
     (<div className="w-full">
